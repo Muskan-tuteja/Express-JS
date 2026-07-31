a852d38a69e9060b3e7098f43a87af120507b1e3
Muskan tuteja <muskantuteja714@gmail.com>
2026-08-01 00:44:49 +0530

    import and export define

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express')
+// const express = require('express')
+
+import express from 'express'
+
+import page from './page.js'
+import about from './about.js'
 
 const app = express()
 
 app.get("",(req,res)=>{
-    res.send("<h1>Home pahe</>")
+    res.send(page())
 
 })
 app.get("/about",(req,res)=>{
-    res.send("<h1>About Page</>")
+    res.send(about())
 
 })
 app.get("",(req,res)=>{
