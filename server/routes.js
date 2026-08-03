1bcebbcbf45d69e76e857682422569ffae64598a
Muskan tuteja <muskantuteja714@gmail.com>
2026-08-03 22:55:07 +0530

    routes

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,22 @@
 import express from "express";
 const app = express();
 
+function checkAgeRouterMiddleware(req,res,next){
+  console.log(req,res,next);
+  
+  if(!req.query.age || req.query.age<18){
+    res.send("You are not is allowed to used this website")
+
+  }else{
+    next()
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
-app.get("/Login", (req, res) => {
+app.get("/Login", checkAgeRouterMiddleware, (req, res) => {
   res.send("<h1>Login The page</h1>");
 });
 app.get("/about", (req, res) => {
