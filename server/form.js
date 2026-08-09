3f10c887eeebfe0128f444ddd1ed9769d56f0372
Muskan tuteja <muskantuteja714@gmail.com>
2026-08-09 22:19:27 +0530

    creted from file help the ejs

---
+++ server/form.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine','ejs')
+
+app.get('/add-user', (req,res)=>{
+    res.render('addUser')
+
+})
+app.post('/submit-user', (req,res)=>{
+    res.render('submitUser',req.body)
+
+})
+app.listen(8900)
