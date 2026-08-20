37c6cdec74681b2799e09a1d79f6c9147402e8ee
Muskan tuteja <muskantuteja714@gmail.com>
2026-08-20 22:01:36 +0530

    created the mongodb

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { MongoClient } from 'mongodb'
 
-const dbName = "school"
+const dbName = "college"
 const url = "mongodb://localhost:27017"
 
 const client = new MongoClient(url)
